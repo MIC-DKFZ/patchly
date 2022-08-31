dc366d6472f15b069462c421c3ec19a977b6b58c
Karol Gotkowski <karol.gotkowski@dkfz-heidelberg.de>
2022-08-31 16:34:32 +0200

    Improved tests

--- tests/test_basic_grid_sampler.py
+++ tests/test_basic_grid_sampler.py
@@ -6,54 +6,137 @@ from samplify.slicer import slicer
 
 
 class TestBasicGridSampler(unittest.TestCase):
+    def setUp(self) -> None:
+        self.max_value = 100000
+        self.image_size_multiple = 10
+        self.enable_prints = False
+
     def test_without_overlap_without_remainder_2d(self):
-        self._test_without_overlap_without_remainder(spatial_dims=2, patch_size_range=(25000, 250000), image_size_multiple=(1, 25000))
+        self._test_without_overlap_without_remainder(spatial_dims=2, patch_size_range=(self.max_value, self.max_value), image_size_multiple=(1, self.image_size_multiple))
 
     def test_without_overlap_with_remainder_2d(self):
-        self._test_without_overlap_with_remainder(spatial_dims=2, patch_size_range=(25000, 250000), image_size_max=2500000)
+        self._test_without_overlap_with_remainder(spatial_dims=2, patch_size_range=(self.max_value, self.max_value*10), image_size_multiple=(1, self.image_size_multiple))
 
     def test_with_overlap_without_remainder_2d(self):
-        self._test_with_overlap_without_remainder(spatial_dims=2, patch_overlap_size_range=(25000, 250000), patch_size_range=(1, 100), image_size_multiple=(1, 25000))
+        self._test_with_overlap_without_remainder(spatial_dims=2, patch_overlap_size_range=(self.max_value, self.max_value*10), patch_size_range=(1, 100), image_size_multiple=(1, self.image_size_multiple))
 
     def test_with_overlap_with_remainder_2d(self):
-        self._test_with_overlap_with_remainder(spatial_dims=2, patch_overlap_size_range=(25000, 250000), patch_size_range=(1, 100), image_size_max=2500000)
+        self._test_with_overlap_with_remainder(spatial_dims=2, patch_overlap_size_range=(self.max_value, self.max_value*10), patch_size_range=(1, 100), image_size_multiple=(1, self.image_size_multiple))
 
     def test_without_overlap_without_remainder_3d(self):
-        self._test_without_overlap_without_remainder(spatial_dims=3, patch_size_range=(25000, 250000), image_size_multiple=(1, 25000))
+        self._test_without_overlap_without_remainder(spatial_dims=3, patch_size_range=(self.max_value, self.max_value*10), image_size_multiple=(1, self.image_size_multiple))
 
     def test_without_overlap_with_remainder_3d(self):
-        self._test_without_overlap_with_remainder(spatial_dims=3, patch_size_range=(25000, 250000), image_size_max=2500000)
+        self._test_without_overlap_with_remainder(spatial_dims=3, patch_size_range=(self.max_value, self.max_value*10), image_size_multiple=(1, self.image_size_multiple))
 
     def test_with_overlap_without_remainder_3d(self):
-        self._test_with_overlap_without_remainder(spatial_dims=3, patch_overlap_size_range=(25000, 250000), patch_size_range=(1, 100), image_size_multiple=(1, 25000))
+        self._test_with_overlap_without_remainder(spatial_dims=3, patch_overlap_size_range=(self.max_value, self.max_value*10), patch_size_range=(1, 100), image_size_multiple=(1, self.image_size_multiple))
 
     def test_with_overlap_with_remainder_3d(self):
-        self._test_with_overlap_with_remainder(spatial_dims=3, patch_overlap_size_range=(25000, 250000), patch_size_range=(1, 100), image_size_max=2500000)
+        self._test_with_overlap_with_remainder(spatial_dims=3, patch_overlap_size_range=(self.max_value, self.max_value*10), patch_size_range=(1, 100), image_size_multiple=(1, self.image_size_multiple))
 
     def test_without_overlap_without_remainder_Nd(self):
         spatial_dims = random.randint(1, 5)
-        self._test_without_overlap_without_remainder(spatial_dims=spatial_dims, patch_size_range=(25000, 250000), image_size_multiple=(1, 25000))
+        self._test_without_overlap_without_remainder(spatial_dims=spatial_dims, patch_size_range=(self.max_value, self.max_value*10), image_size_multiple=(1, self.image_size_multiple))
 
     def test_without_overlap_with_remainder_Nd(self):
         spatial_dims = random.randint(1, 5)
-        self._test_without_overlap_with_remainder(spatial_dims=spatial_dims, patch_size_range=(25000, 250000), image_size_max=2500000)
+        self._test_without_overlap_with_remainder(spatial_dims=spatial_dims, patch_size_range=(self.max_value, self.max_value*10), image_size_multiple=(1, self.image_size_multiple))
 
     def test_with_overlap_without_remainder_Nd(self):
         spatial_dims = random.randint(1, 5)
-        self._test_with_overlap_without_remainder(spatial_dims=spatial_dims, patch_overlap_size_range=(25000, 250000), patch_size_range=(1, 100), image_size_multiple=(1, 25000))
+        self._test_with_overlap_without_remainder(spatial_dims=spatial_dims, patch_overlap_size_range=(self.max_value, self.max_value), patch_size_range=(1, 100), image_size_multiple=(1, self.image_size_multiple))
 
     def test_with_overlap_with_remainder_Nd(self):
         spatial_dims = random.randint(1, 5)
-        self._test_with_overlap_with_remainder(spatial_dims=spatial_dims, patch_overlap_size_range=(25000, 250000), patch_size_range=(1, 100), image_size_max=2500000)
+        self._test_with_overlap_with_remainder(spatial_dims=spatial_dims, patch_overlap_size_range=(self.max_value, self.max_value), patch_size_range=(1, 100), image_size_multiple=(1, self.image_size_multiple))
 
     def test_without_channel(self):
-        pass
+        self._test_without_overlap_without_remainder(spatial_dims=2, patch_size_range=(self.max_value, self.max_value), image_size_multiple=(1, self.image_size_multiple))
 
     def test_channel_first(self):
-        pass
+        spatial_dims = 2
+        patch_size_range = (self.max_value, self.max_value)
+        image_size_multiple = (1, self.image_size_multiple)
+        max_channels = 5
+        channel_first = True
+
+        patch_size_range = tuple(self._adjust_to_dim(patch_size_range, spatial_dims))
+        image_size_multiple = self._adjust_to_dim(image_size_multiple, spatial_dims)
+
+        patch_size = [random.randint(*patch_size_range) for i in range(spatial_dims)]
+        image_size = [patch_size[i] * random.randint(*image_size_multiple) for i in range(spatial_dims)]
+
+        num_channels = random.randint(1, max_channels)
+        if channel_first:
+            image = np.random.random((num_channels, *image_size))
+        else:
+            image = np.random.random((*image_size, num_channels))
+
+        # Test with image
+        result = np.zeros_like(image)
+        sampler = BasicGridSampler(image=image, image_size=image_size, patch_size=patch_size, channel_first=channel_first)
+        if self.enable_prints:
+            print("Dim: {}, sampler len: {}, image_size: {}, patch_size: {}".format(spatial_dims, len(sampler), image_size, patch_size))
+
+        for patch, patch_indices in sampler:
+            slices = self.get_slices(result, image_size, patch_indices, channel_first)
+            result[slicer(result, slices)] = 1
+            assert np.array_equal(patch, image[slicer(image, slices)])
+
+        assert np.sum(result) == result.size
+
+        # Test without image
+        result = np.zeros_like(image)
+        sampler = BasicGridSampler(image_size=image_size, patch_size=patch_size)
+
+        for patch_indices in sampler:
+            slices = self.get_slices(result, image_size, patch_indices, channel_first)
+            result[slicer(result, slices)] = 1
+
+        assert np.sum(result) == result.size
 
     def test_channel_last(self):
-        pass
+        spatial_dims = 2
+        patch_size_range = (self.max_value, self.max_value)
+        image_size_multiple = (1, self.image_size_multiple)
+        max_channels = 5
+        channel_first = False
+
+        patch_size_range = tuple(self._adjust_to_dim(patch_size_range, spatial_dims))
+        image_size_multiple = self._adjust_to_dim(image_size_multiple, spatial_dims)
+
+        patch_size = [random.randint(*patch_size_range) for i in range(spatial_dims)]
+        image_size = [patch_size[i] * random.randint(*image_size_multiple) for i in range(spatial_dims)]
+
+        num_channels = random.randint(1, max_channels)
+        if channel_first:
+            image = np.random.random((num_channels, *image_size))
+        else:
+            image = np.random.random((*image_size, num_channels))
+
+        # Test with image
+        result = np.zeros_like(image)
+        sampler = BasicGridSampler(image=image, image_size=image_size, patch_size=patch_size, channel_first=channel_first)
+        if self.enable_prints:
+            print("Dim: {}, sampler len: {}, image_size: {}, patch_size: {}".format(spatial_dims, len(sampler), image_size, patch_size))
+
+        for patch, patch_indices in sampler:
+            slices = self.get_slices(result, image_size, patch_indices, channel_first)
+            result[slicer(result, slices)] = 1
+            assert np.array_equal(patch, image[slicer(image, slices)])
+
+        assert np.sum(result) == result.size
+
+        # Test without image
+        result = np.zeros_like(image)
+        sampler = BasicGridSampler(image_size=image_size, patch_size=patch_size)
+
+        for patch_indices in sampler:
+            slices = self.get_slices(result, image_size, patch_indices, channel_first)
+            result[slicer(result, slices)] = 1
+
+        assert np.sum(result) == result.size
 
     def test_batch_and_channel_dim(self):
         pass
@@ -87,9 +170,11 @@ class TestBasicGridSampler(unittest.TestCase):
         # Test with image
         result = np.zeros_like(image)
         sampler = BasicGridSampler(image=image, image_size=image_size, patch_size=patch_size)
+        if self.enable_prints:
+            print("Dim: {}, sampler len: {}, image_size: {}, patch_size: {}".format(spatial_dims, len(sampler), image_size, patch_size))
 
         for patch, patch_indices in sampler:
-            result[slicer(image, patch_indices)] = 1
+            result[slicer(result, patch_indices)] = 1
             assert np.array_equal(patch, image[slicer(image, patch_indices)])
 
         assert np.sum(result) == result.size
@@ -99,25 +184,27 @@ class TestBasicGridSampler(unittest.TestCase):
         sampler = BasicGridSampler(image_size=image_size, patch_size=patch_size)
 
         for patch_indices in sampler:
-            result[slicer(image, patch_indices)] = 1
+            result[slicer(result, patch_indices)] = 1
 
         assert np.sum(result) == result.size
 
-    def _test_without_overlap_with_remainder(self, spatial_dims, patch_size_range, image_size_max):
+    def _test_without_overlap_with_remainder(self, spatial_dims, patch_size_range, image_size_multiple):
         patch_size_range = tuple(self._adjust_to_dim(patch_size_range, spatial_dims))
-        image_size_max = self._adjust_to_dim(image_size_max, spatial_dims)
+        image_size_multiple = self._adjust_to_dim(image_size_multiple, spatial_dims)
 
         patch_size = [random.randint(*patch_size_range) for i in range(spatial_dims)]
-        image_size = [random.randint(patch_size[i], image_size_max) for i in range(spatial_dims)]
+        image_size = [random.randint(patch_size[i], patch_size[i]*image_size_multiple[1]) for i in range(spatial_dims)]
         image = np.random.random(image_size)
         quotient_size = np.floor_divide(image_size, patch_size) * patch_size
 
         # Test with image
         result = np.zeros(quotient_size)
         sampler = BasicGridSampler(image=image, image_size=image_size, patch_size=patch_size)
+        if self.enable_prints:
+            print("Dim: {}, sampler len: {}, image_size: {}, patch_size: {}".format(spatial_dims, len(sampler), image_size, patch_size))
 
         for patch, patch_indices in sampler:
-            result[slicer(image, patch_indices)] = 1
+            result[slicer(result, patch_indices)] = 1
             assert np.array_equal(patch, image[slicer(image, patch_indices)])
 
         assert np.sum(result) == result.size
@@ -127,7 +214,7 @@ class TestBasicGridSampler(unittest.TestCase):
         sampler = BasicGridSampler(image_size=image_size, patch_size=patch_size)
 
         for patch_indices in sampler:
-            result[slicer(image, patch_indices)] = 1
+            result[slicer(result, patch_indices)] = 1
 
         assert np.sum(result) == result.size
 
@@ -144,9 +231,11 @@ class TestBasicGridSampler(unittest.TestCase):
         # Test with image
         result = np.zeros_like(image)
         sampler = BasicGridSampler(image=image, image_size=image_size, patch_size=patch_size, patch_overlap=patch_overlap)
+        if self.enable_prints:
+            print("Dim: {}, sampler len: {}, image_size: {}, patch_size: {}, patch_overlap: {}".format(spatial_dims, len(sampler), image_size, patch_size, patch_overlap))
 
         for patch, patch_indices in sampler:
-            result[slicer(image, patch_indices)] = 1
+            result[slicer(result, patch_indices)] = 1
             assert np.array_equal(patch, image[slicer(image, patch_indices)])
 
         assert np.sum(result) == result.size
@@ -156,27 +245,29 @@ class TestBasicGridSampler(unittest.TestCase):
         sampler = BasicGridSampler(image_size=image_size, patch_size=patch_size, patch_overlap=patch_overlap)
 
         for patch_indices in sampler:
-            result[slicer(image, patch_indices)] = 1
+            result[slicer(result, patch_indices)] = 1
 
         assert np.sum(result) == result.size
 
-    def _test_with_overlap_with_remainder(self, spatial_dims, patch_overlap_size_range, patch_size_range, image_size_max):
+    def _test_with_overlap_with_remainder(self, spatial_dims, patch_overlap_size_range, patch_size_range, image_size_multiple):
         patch_overlap_size_range = tuple(self._adjust_to_dim(patch_overlap_size_range, spatial_dims))
         patch_size_range = tuple(self._adjust_to_dim(patch_size_range, spatial_dims))
-        image_size_max = self._adjust_to_dim(image_size_max, spatial_dims)
+        image_size_multiple = self._adjust_to_dim(image_size_multiple, spatial_dims)
 
         patch_overlap = [random.randint(*patch_overlap_size_range) for i in range(spatial_dims)]
         patch_size = [patch_overlap[i] * random.randint(*patch_size_range) for i in range(spatial_dims)]
-        image_size = [random.randint(patch_size[i], image_size_max) for i in range(spatial_dims)]
+        image_size = [random.randint(patch_size[i], patch_size[i] * image_size_multiple[1]) for i in range(spatial_dims)]
         image = np.random.random(image_size)
         quotient_size = np.floor_divide(image_size, patch_size) * patch_size
 
         # Test with image
         result = np.zeros(quotient_size)
         sampler = BasicGridSampler(image=image, image_size=image_size, patch_size=patch_size, patch_overlap=patch_overlap)
+        if self.enable_prints:
+            print("Dim: {}, sampler len: {}, image_size: {}, patch_size: {}, patch_overlap: {}".format(spatial_dims, len(sampler), image_size, patch_size, patch_overlap))
 
         for patch, patch_indices in sampler:
-            result[slicer(image, patch_indices)] = 1
+            result[slicer(result, patch_indices)] = 1
             assert np.array_equal(patch, image[slicer(image, patch_indices)])
 
         assert np.sum(result) == result.size
@@ -186,9 +277,19 @@ class TestBasicGridSampler(unittest.TestCase):
         sampler = BasicGridSampler(image_size=image_size, patch_size=patch_size, patch_overlap=patch_overlap)
 
         for patch_indices in sampler:
-            result[slicer(image, patch_indices)] = 1
+            result[slicer(result, patch_indices)] = 1
 
         assert np.sum(result) == result.size
 
     def _adjust_to_dim(self, arr, spatial_dims):
-        return np.uint32(np.rint(np.power(arr, 1/spatial_dims)))
+        return np.int64(np.rint(np.power(arr, 1/spatial_dims)))
+
+    def get_slices(self, image, image_size, patch_indices, channel_first):
+        non_image_dims = len(image.shape) - len(image_size)
+        if channel_first:
+            slices = [None] * non_image_dims
+            slices.extend([index_pair.tolist() for index_pair in patch_indices])
+        else:
+            slices = [index_pair.tolist() for index_pair in patch_indices]
+            slices.extend([None] * non_image_dims)
+        return slices

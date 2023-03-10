91b23f98d6b3b200aeb4b3e359389abf46ec18ec
Karol Gotkowski <karol.gotkowski@dkfz-heidelberg.de>
2023-03-10 15:19:07 +0100

    Test bugfix

--- tests/test_chunk_grid_aggregator.py
+++ tests/test_chunk_grid_aggregator.py
@@ -163,22 +163,19 @@ class TestChunkGridAggregator(unittest.TestCase):
         patch_overlap = (5,)
         chunk_size = (20,)
         spatial_size = (40,)
-        image = np.random.random(spatial_size)
+        image = np.random.random(spatial_size).astype(np.float32)
 
-        expected_output = np.zeros_like(image)
-        weights = self.create_gaussian_weights(np.asarray(patch_size))
-        weight_map = np.zeros_like(image)
+        expected_output = np.zeros_like(image, dtype=np.float32)
+        weights = self.create_gaussian_weights(np.asarray(patch_size)).astype(np.float32)
+        weight_map = np.zeros_like(image, dtype=np.float32)
 
         for i, index in enumerate(range(0, spatial_size[0]-patch_overlap[0], patch_overlap[0])):
-            expected_output[index:index+patch_size[0]] += image[index:index+patch_size[0]] * weights * i
+            expected_output[index:index+patch_size[0]] += image[index:index+patch_size[0]] * weights * 2
             weight_map[index:index+patch_size[0]] += weights
         expected_output /= weight_map
         expected_output = np.nan_to_num(expected_output)
 
-        output1, output2 = self._test_aggregator(image, spatial_size, patch_size, chunk_size, patch_overlap=patch_overlap, weights='gaussian', multiply_patch_by_index=True)
-
-        diff = np.abs(expected_output - output1)
-        diff_sum = np.sum(diff)
+        output1, output2 = self._test_aggregator(image, spatial_size, patch_size, chunk_size, patch_overlap=patch_overlap, weights='gaussian', multiply_elements_by_two=True)
 
         np.testing.assert_almost_equal(output1, expected_output, decimal=6)
         np.testing.assert_almost_equal(output2, expected_output, decimal=6)
@@ -199,21 +196,21 @@ class TestChunkGridAggregator(unittest.TestCase):
     #
     #     self.assertRaises(RuntimeError, GridSampler, image=image, spatial_size=spatial_size, patch_size=patch_size, patch_overlap=patch_overlap, mode="sample_edge")
 
-    def _test_aggregator(self, image, spatial_size, patch_size, chunk_size, patch_overlap=None, spatial_first=True, output=None, weights='avg', multiply_patch_by_index=False):
+    def _test_aggregator(self, image, spatial_size, patch_size, chunk_size, patch_overlap=None, spatial_first=True, output=None, weights='avg', multiply_elements_by_two=False):
         # Test with output size
         sampler = GridSampler(image=copy.deepcopy(image), spatial_size=spatial_size, patch_size=patch_size, patch_overlap=patch_overlap, chunk_size=chunk_size, spatial_first=spatial_first, mode="sample_edge")
         aggregator = Aggregator(sampler=sampler, output_size=image.shape, weights=weights)
 
         for i, (patch, patch_indices, chunk_id) in enumerate(sampler):
-            if multiply_patch_by_index:
-                _patch = patch * i
+            if multiply_elements_by_two:
+                _patch = patch * 2
             else:
                 _patch = patch
             aggregator.append(_patch, patch_indices, chunk_id)
 
         output1 = aggregator.get_output()
 
-        if not multiply_patch_by_index:
+        if not multiply_elements_by_two:
             np.testing.assert_almost_equal(np.array(image), np.array(output1), decimal=6)
 
         # Test without output array
@@ -223,15 +220,15 @@ class TestChunkGridAggregator(unittest.TestCase):
         aggregator = Aggregator(sampler=sampler, output=output, weights=weights)
 
         for i, (patch, patch_indices, chunk_id) in enumerate(sampler):
-            if multiply_patch_by_index:
-                _patch = patch * i
+            if multiply_elements_by_two:
+                _patch = patch * 2
             else:
                 _patch = patch
             aggregator.append(_patch, patch_indices, chunk_id)
 
         output2 = aggregator.get_output()
 
-        if not multiply_patch_by_index:
+        if not multiply_elements_by_two:
             np.testing.assert_almost_equal(np.array(image), np.array(output2), decimal=6)
 
         return output1, output2

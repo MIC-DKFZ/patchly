d2af290f33171285c612119759f154c7599f8b27
Karol Gotkowski <karol.gotkowski@dkfz-heidelberg.de>
2023-08-03 16:30:39 +0200

    feat(aggregator): Check if spatial size of given output is equal to given spatial size

--- samplify/aggregator.py
+++ samplify/aggregator.py
@@ -13,25 +13,51 @@ import numpy.typing as npt
 class Aggregator:
     def __init__(self, sampler: GridSampler, output_size: Optional[Union[Tuple, npt.ArrayLike]] = None, output: Optional[npt.ArrayLike] = None, weights: Union[str, Callable] = 'avg', softmax_dim: Optional[int] = None, spatial_first: bool = True):
         self.sampler = sampler
+        self.spatial_size = sampler.spatial_size
+        self.patch_size = sampler.patch_size
+        self.patch_overlap = sampler.patch_overlap
+        self.chunk_size = sampler.chunk_size
         self.spatial_first = spatial_first
-        self.aggregator = self.set_aggregator(sampler, output_size, output, weights, softmax_dim)
+        self.mode = sampler.mode
+        self.softmax_dim = softmax_dim
+        self.output = self.set_output(output, output_size)
+        self.check_sanity()
+        self.aggregator = self.set_aggregator(self.output, weights, self.softmax_dim)
 
-    def set_aggregator(self, sampler, output_size, output, weights, softmax_dim):
-        mode = sampler.mode
-        chunk_size = sampler.chunk_size
-        if mode.startswith('sample_') and chunk_size is None:
-            sampler = _Aggregator(spatial_size=sampler.spatial_size, patch_size=sampler.patch_size, output_size=output_size,
+    def set_output(self, output, output_size):
+        if output is None and output_size is not None:
+            output = np.zeros(output_size, dtype=np.float32)
+            return output
+        elif output is None and output_size is None:
+            raise RuntimeError("Either the output array-like data or the output size must be given.")
+        elif output is not None and output_size is not None and output.shape != output_size:
+            raise RuntimeError("The variable output_size must be equal to the output shape if both are given. Only one of the two must be given.")
+        return output
+
+    def check_sanity(self):
+        if self.softmax_dim is not None and not issubclass(type(self), _ChunkAggregator):
+            raise NotImplementedError("Softmax reduction is not supported on unchunked images.")
+        if not hasattr(self.output, '__getitem__'):
+            raise RuntimeError("The given output is not ArrayLike.")
+        if self.spatial_first and (self.output.shape[:len(self.spatial_size)] != tuple(self.spatial_size)):
+            raise RuntimeError("The spatial size of the given output {} is unequal to the given spatial size {}.".format(self.output.shape[:len(self.spatial_size)], self.spatial_size))
+        if (not self.spatial_first) and (self.output.shape[-len(self.spatial_size):] != tuple(self.spatial_size)):
+            raise RuntimeError("The spatial size of the given output {} is unequal to the given spatial size {}.".format(self.output.shape[-len(self.spatial_size):], self.spatial_size))
+
+    def set_aggregator(self, output, weights, softmax_dim):
+        if self.mode.startswith('sample_') and self.chunk_size is None:
+            aggregator = _Aggregator(spatial_size=self.spatial_size, patch_size=self.patch_size,
                                   output=output, weights=weights, spatial_first=self.spatial_first, softmax_dim=softmax_dim)
-        elif mode.startswith('sample_') and chunk_size is not None:
-            sampler = _ChunkAggregator(spatial_size=sampler.spatial_size, patch_size=sampler.patch_size, patch_overlap=sampler.patch_overlap, chunk_size=sampler.chunk_size,
-                                       output_size=output_size, output=output, weights=weights, spatial_first=self.spatial_first, softmax_dim=softmax_dim)
-        elif mode.startswith('pad_') and chunk_size is None:
-            raise NotImplementedError("The given sampling mode ({}) is not supported.".format(mode))
-        elif mode.startswith('pad_') and chunk_size is not None:
-            raise NotImplementedError("The given sampling mode ({}) is not supported.".format(mode))
+        elif self.mode.startswith('sample_') and self.chunk_size is not None:
+            aggregator = _ChunkAggregator(spatial_size=self.spatial_size, patch_size=self.patch_size, patch_overlap=self.patch_overlap, chunk_size=self.chunk_size,
+                                       output=output, weights=weights, spatial_first=self.spatial_first, softmax_dim=softmax_dim)
+        elif self.mode.startswith('pad_') and self.chunk_size is None:
+            raise NotImplementedError("The given sampling mode ({}) is not supported.".format(self.mode))
+        elif self.mode.startswith('pad_') and self.chunk_size is not None:
+            raise NotImplementedError("The given sampling mode ({}) is not supported.".format(self.mode))
         else:
-            raise NotImplementedError("The given sampling mode ({}) is not supported.".format(mode))
-        return sampler
+            raise NotImplementedError("The given sampling mode ({}) is not supported.".format(self.mode))
+        return aggregator
 
     def append(self, patch, patch_indices, chunk_id=None):
         self.aggregator.append(patch, patch_indices, chunk_id)
@@ -50,7 +76,7 @@ class Aggregator:
 
 
 class _Aggregator:
-    def __init__(self, spatial_size: Union[Tuple, npt.ArrayLike], patch_size: Union[Tuple, npt.ArrayLike], output_size: Optional[Union[Tuple, npt.ArrayLike]] = None,
+    def __init__(self, spatial_size: Union[Tuple, npt.ArrayLike], patch_size: Union[Tuple, npt.ArrayLike],
                  output: Optional[npt.ArrayLike] = None, weights: Union[str, Callable] = 'avg', spatial_first: bool = True, softmax_dim: Optional[int] = None):
         """
         Aggregator to assemble an image with continuous content from patches. The content of overlapping patches is averaged.
@@ -63,30 +89,11 @@ class _Aggregator:
         """
         self.spatial_size = np.asarray(spatial_size)
         self.patch_size = np.asarray(patch_size)
-        self.output = self.set_output(output, output_size)
+        self.output = output
         self.spatial_first = spatial_first
         self.softmax_dim = softmax_dim
         self.weight_patch, self.weight_map = self.set_weights(weights)
         self.computed_inplace = False
-        self.check_sanity()
-
-    def set_output(self, output, output_size):
-        if output is None and output_size is not None:
-            output = np.zeros(output_size, dtype=np.float32)
-            return output
-        elif output is None and output_size is None:
-            raise RuntimeError("Either the output array-like data or the output size must be given.")
-        elif output is not None and output_size is not None and output.shape != output_size:
-            raise RuntimeError("The variable output_size must be equal to the output shape if both are given. Only one of the two must be given.")
-        return output
-
-    def check_sanity(self):
-        if np.any(self.patch_size > self.spatial_size):
-            raise RuntimeError("The patch size ({}) cannot be larger than the spatial size ({}).".format(self.patch_size, self.spatial_size))
-        if self.softmax_dim is not None and not issubclass(type(self), _ChunkAggregator):
-            raise NotImplementedError("Softmax reduction is not supported on unchunked images.")
-        # TODO: Check spatial_size == output.shape. Consider non-spatial dims and spatial_first
-        # TODO: Check spatial_size == output_size. Consider non-spatial dims and spatial_first
 
     def set_weights(self, weights, weight_map_size=None):
         if weights == 'avg':
@@ -184,7 +191,7 @@ class _Aggregator:
 
 class _ChunkAggregator(_Aggregator):
     def __init__(self, spatial_size: Union[Tuple, npt.ArrayLike], patch_size: Union[Tuple, npt.ArrayLike], patch_overlap: Union[Tuple, npt.ArrayLike], chunk_size: Union[Tuple, npt.ArrayLike],
-                 output_size: Optional[Union[Tuple, npt.ArrayLike]] = None, output: Optional[npt.ArrayLike] = None, weights: Union[str, Callable] = 'avg', spatial_first: bool = True,
+                 output: Optional[npt.ArrayLike] = None, weights: Union[str, Callable] = 'avg', spatial_first: bool = True,
                  softmax_dim: Optional[int] = None, mode: str = 'sample_edge'):
         """
         Weighted aggregator to assemble an image with continuous content from patches. Returns the maximum class at each position of the image. The content of overlapping patches is gaussian-weighted by default.
@@ -196,7 +203,7 @@ class _ChunkAggregator(_Aggregator):
         :param weights: A weight map of size patch_size that should be used for weighting the importance of each value in a patch. Default is a gaussian weight map.
         :param low_memory_mode: Reduces memory consumption by more than 50% in comparison to the normal WeightedAggregator and Aggregator. However, the prediction quality is slightly reduced.
         """
-        super().__init__(spatial_size=spatial_size, patch_size=patch_size, output_size=output_size, output=output, weights=weights, spatial_first=spatial_first, softmax_dim=softmax_dim)
+        super().__init__(spatial_size=spatial_size, patch_size=patch_size, output=output, weights=weights, spatial_first=spatial_first, softmax_dim=softmax_dim)
         self.patch_overlap = patch_overlap
         self.chunk_size = chunk_size
         self.chunk_dtype = self.set_chunk_dtype()

69c0134d4cf6203fc23f56e646eb7c5c3de9748a
Karol Gotkowski <karol.gotkowski@dkfz-heidelberg.de>
2023-08-11 13:28:30 +0200

    refactor(aggregator): Moved weight creation

--- samplify/aggregator.py
+++ samplify/aggregator.py
@@ -11,7 +11,8 @@ import numpy.typing as npt
 
 
 class Aggregator:
-    def __init__(self, sampler: GridSampler, output_size: Optional[Union[Tuple, npt.ArrayLike]] = None, output: Optional[npt.ArrayLike] = None, weights: Union[str, Callable] = 'avg', softmax_dim: Optional[int] = None, spatial_first: bool = True):
+    def __init__(self, sampler: GridSampler, output_size: Optional[Union[Tuple, npt.ArrayLike]] = None, output: Optional[npt.ArrayLike] = None, 
+                 weights: Union[str, Callable] = 'avg', softmax_dim: Optional[int] = None, spatial_first: bool = True):
         self.sampler = sampler
         self.spatial_size = sampler.spatial_size
         self.patch_size = sampler.patch_size
@@ -21,8 +22,9 @@ class Aggregator:
         self.mode = sampler.mode
         self.softmax_dim = softmax_dim
         self.output = self.set_output(output, output_size)
+        self.weight_patch, self.weight_map = self.set_weights(weights)
         self.check_sanity()
-        self.aggregator = self.set_aggregator(self.output, weights, self.softmax_dim)
+        self.aggregator = self.set_aggregator(self.output, self.softmax_dim)
 
     def set_output(self, output, output_size):
         if output is None and output_size is not None:
@@ -34,6 +36,41 @@ class Aggregator:
             raise RuntimeError("The variable output_size must be equal to the output shape if both are given. Only one of the two must be given.")
         return output
 
+    def set_weights(self, weights):
+        if weights == 'avg':
+            weight_patch = np.ones(self.patch_size, dtype=np.uint8)
+        elif weights == 'gaussian':
+            weight_patch = self.create_gaussian_weights(self.patch_size)
+        elif callable(weights):
+            weight_patch = weights
+        else:
+            raise RuntimeError("The given type of weights is not supported.")
+        weight_patch = utils.broadcast_to(weight_patch, utils.add_non_spatial_dims(weight_patch.shape, self.output.shape, self.spatial_size, self.spatial_first), self.spatial_first)
+
+        if self.softmax_dim is None:
+            if self.chunk_size is None:
+                weight_map_size = self.spatial_size
+            else:
+                weight_map_size = self.chunk_size
+            if weights == 'avg':
+                weight_map = np.zeros(weight_map_size, dtype=np.uint16)
+            else:
+                weight_map = np.zeros(weight_map_size, dtype=np.float32)
+            weight_map = utils.broadcast_to(weight_map, utils.add_non_spatial_dims(weight_map.shape, self.output.shape, self.spatial_size, self.spatial_first), self.spatial_first)
+        else:
+            weight_map = None
+        return weight_patch, weight_map
+    
+    def create_gaussian_weights(self, size):
+        sigma_scale = 1. / 8
+        sigmas = size * sigma_scale
+        center_coords = size // 2
+        tmp = np.zeros(size)
+        tmp[tuple(center_coords)] = 1
+        gaussian_weights = gaussian_filter(tmp, sigmas, 0, mode='constant', cval=0)
+        gaussian_weights[gaussian_weights == 0] = np.min(gaussian_weights[gaussian_weights != 0])
+        return gaussian_weights
+
     def check_sanity(self):
         if self.softmax_dim is not None and not issubclass(type(self), _ChunkAggregator):
             raise NotImplementedError("Softmax reduction is not supported on unchunked images.")
@@ -44,13 +81,13 @@ class Aggregator:
         if (not self.spatial_first) and (self.output.shape[-len(self.spatial_size):] != tuple(self.spatial_size)):
             raise RuntimeError("The spatial size of the given output {} is unequal to the given spatial size {}.".format(self.output.shape[-len(self.spatial_size):], self.spatial_size))
 
-    def set_aggregator(self, output, weights, softmax_dim):
+    def set_aggregator(self, output, softmax_dim):
         if self.mode.startswith('sample_') and self.chunk_size is None:
             aggregator = _Aggregator(spatial_size=self.spatial_size, patch_size=self.patch_size,
-                                  output=output, weights=weights, spatial_first=self.spatial_first, softmax_dim=softmax_dim)
+                                  output=output, spatial_first=self.spatial_first, softmax_dim=softmax_dim, weight_patch=self.weight_patch, weight_map=self.weight_map)
         elif self.mode.startswith('sample_') and self.chunk_size is not None:
             aggregator = _ChunkAggregator(spatial_size=self.spatial_size, patch_size=self.patch_size, patch_overlap=self.patch_overlap, chunk_size=self.chunk_size,
-                                       output=output, weights=weights, spatial_first=self.spatial_first, softmax_dim=softmax_dim)
+                                       output=output, spatial_first=self.spatial_first, softmax_dim=softmax_dim, weight_patch=self.weight_patch, weight_map=self.weight_map)
         elif self.mode.startswith('pad_') and self.chunk_size is None:
             raise NotImplementedError("The given sampling mode ({}) is not supported.".format(self.mode))
         elif self.mode.startswith('pad_') and self.chunk_size is not None:
@@ -77,7 +114,8 @@ class Aggregator:
 
 class _Aggregator:
     def __init__(self, spatial_size: Union[Tuple, npt.ArrayLike], patch_size: Union[Tuple, npt.ArrayLike],
-                 output: Optional[npt.ArrayLike] = None, weights: Union[str, Callable] = 'avg', spatial_first: bool = True, softmax_dim: Optional[int] = None):
+                 output: Optional[npt.ArrayLike] = None, spatial_first: bool = True, softmax_dim: Optional[int] = None,
+                 weight_patch: npt.ArrayLike = None, weight_map: npt.ArrayLike = None):
         """
         Aggregator to assemble an image with continuous content from patches. The content of overlapping patches is averaged.
         Can be used in conjunction with the GridSampler during inference to assemble the image-predictions from the patch-predictions.
@@ -92,31 +130,9 @@ class _Aggregator:
         self.output = output
         self.spatial_first = spatial_first
         self.softmax_dim = softmax_dim
-        self.weight_patch, self.weight_map = self.set_weights(weights)
+        self.weight_patch = weight_patch
+        self.weight_map = weight_map
         self.computed_inplace = False
-
-    def set_weights(self, weights, weight_map_size=None):
-        if weights == 'avg':
-            weight_patch = np.ones(self.patch_size, dtype=np.uint8)
-        elif weights == 'gaussian':
-            weight_patch = self.create_gaussian_weights(self.patch_size)
-        elif callable(weights):
-            weight_patch = weights
-        else:
-            raise RuntimeError("The given type of weights is not supported.")
-        weight_patch = self.broadcast_to(weight_patch, self.add_non_spatial_dims(weight_patch.shape, self.output.shape))
-
-        if self.softmax_dim is None:
-            if weight_map_size is None:
-                weight_map_size = self.spatial_size
-            if weights == 'avg':
-                weight_map = np.zeros(weight_map_size, dtype=np.uint16)
-            else:
-                weight_map = np.zeros(weight_map_size, dtype=np.float32)
-            weight_map = self.broadcast_to(weight_map, self.add_non_spatial_dims(weight_map.shape, self.output.shape))
-        else:
-            weight_map = None
-        return weight_patch, weight_map
 
     def append(self, patch, patch_indices, chunk_id):
         """
@@ -153,46 +169,11 @@ class _Aggregator:
                 self.computed_inplace = True
         return output
 
-    def create_gaussian_weights(self, size):
-        sigma_scale = 1. / 8
-        sigmas = size * sigma_scale
-        center_coords = size // 2
-        tmp = np.zeros(size)
-        tmp[tuple(center_coords)] = 1
-        gaussian_weights = gaussian_filter(tmp, sigmas, 0, mode='constant', cval=0)
-        gaussian_weights[gaussian_weights == 0] = np.min(gaussian_weights[gaussian_weights != 0])
-        return gaussian_weights
-
-    def add_non_spatial_dims(self, spatial_data_size_a, data_size_b):
-        non_spatial_dims = len(data_size_b) - len(self.spatial_size)
-        if non_spatial_dims > 0 and self.spatial_first:
-            non_spatial_dims = data_size_b[len(self.spatial_size):]
-            data_size_a = (*spatial_data_size_a, *non_spatial_dims)
-            return data_size_a
-        elif non_spatial_dims > 0:
-            non_spatial_dims = data_size_b[:-len(self.spatial_size)]
-            data_size_a = (*non_spatial_dims, *spatial_data_size_a)
-            return data_size_a
-        else:
-            return spatial_data_size_a
-
-    def broadcast_to(self, data, target_shape):
-        if self.spatial_first:
-            non_spatial_dims = len(target_shape) - len(data.shape)
-            data_reshaped = data
-            for _ in range(non_spatial_dims):
-                data_reshaped = data_reshaped[..., np.newaxis]
-            data_reshaped = np.broadcast_to(data_reshaped, target_shape)
-        else:
-            data_reshaped = np.broadcast_to(data, target_shape)
-        data_reshaped = np.copy(data_reshaped)
-        return data_reshaped
-
 
 class _ChunkAggregator(_Aggregator):
     def __init__(self, spatial_size: Union[Tuple, npt.ArrayLike], patch_size: Union[Tuple, npt.ArrayLike], patch_overlap: Union[Tuple, npt.ArrayLike], chunk_size: Union[Tuple, npt.ArrayLike],
-                 output: Optional[npt.ArrayLike] = None, weights: Union[str, Callable] = 'avg', spatial_first: bool = True,
-                 softmax_dim: Optional[int] = None, mode: str = 'sample_edge'):
+                 output: Optional[npt.ArrayLike] = None, spatial_first: bool = True,
+                 softmax_dim: Optional[int] = None, weight_patch: npt.ArrayLike = None, weight_map: npt.ArrayLike = None, mode: str = 'sample_edge'):
         """
         Weighted aggregator to assemble an image with continuous content from patches. Returns the maximum class at each position of the image. The content of overlapping patches is gaussian-weighted by default.
         Can be used in conjunction with the GridSampler during inference to assemble the image-predictions from the patch-predictions.
@@ -203,11 +184,10 @@ class _ChunkAggregator(_Aggregator):
         :param weights: A weight map of size patch_size that should be used for weighting the importance of each value in a patch. Default is a gaussian weight map.
         :param low_memory_mode: Reduces memory consumption by more than 50% in comparison to the normal WeightedAggregator and Aggregator. However, the prediction quality is slightly reduced.
         """
-        super().__init__(spatial_size=spatial_size, patch_size=patch_size, output=output, weights=weights, spatial_first=spatial_first, softmax_dim=softmax_dim)
+        super().__init__(spatial_size=spatial_size, patch_size=patch_size, output=output, spatial_first=spatial_first, softmax_dim=softmax_dim, weight_patch=weight_patch, weight_map=weight_map)
         self.patch_overlap = patch_overlap
         self.chunk_size = chunk_size
         self.chunk_dtype = self.set_chunk_dtype()
-        self.weight_patch, _ = self.set_weights(weights, self.chunk_size)
         self.mode = mode
         self.compute_indices()
         self.executor = concurrent.futures.ThreadPoolExecutor(max_workers=2)
@@ -270,7 +250,7 @@ class _ChunkAggregator(_Aggregator):
     def process_chunk(self, chunk_id):
         # If they are all not None, create a softmax array of size chunk_size with number classes as channels
         patch_shape = list(self.chunk_patches_dicts[chunk_id].values())[0]["patch"].shape
-        chunk_size = self.add_non_spatial_dims(self.chunk_size, patch_shape)
+        chunk_size = utils.add_non_spatial_dims(self.chunk_size, patch_shape, self.spatial_size, self.spatial_first)
         chunk = np.zeros(chunk_size, dtype=self.chunk_dtype)
         # Weight each patch during insertion
         sampler_offset = self.chunk_sampler_offset[chunk_id].reshape(-1, 1)
@@ -342,7 +322,7 @@ class _ChunkAggregator(_Aggregator):
 #             patch_indices = np.array(np.frombuffer(patch_indices_key, dtype=np.int64), dtype=int).reshape(-1, 2)
 #             patch_indices -= sampler_offset
 #             # patch_indices = np.rint(patch_indices * size_conversion_factor).astype(np.int32)
-#             slices = self.add_non_spatial_dims(image_chunk_softmax, patch_indices)
+#             slices = utils.add_non_spatial_dims(image_chunk_softmax, patch_indices, self.spatial_size, self.spatial_first)
 #             image_chunk_softmax[slicer(image_chunk_softmax, slices)] += patch.astype(image_chunk_softmax.dtype) * self.weight_patch.astype(image_chunk_softmax.dtype)
 #         # Argmax the softmax chunk
 #         image_chunk = image_chunk_softmax.argmax(axis=0).astype(np.uint16)
--- samplify/utils.py
+++ samplify/utils.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def add_non_spatial_indices(image, patch_indices, spatial_size, spatial_first):
     non_spatial_dims = len(image.shape) - len(spatial_size)
     if spatial_first:
@@ -7,3 +10,30 @@ def add_non_spatial_indices(image, patch_indices, spatial_size, spatial_first):
         slices = [[None]] * non_spatial_dims
         slices.extend([index_pair.tolist() for index_pair in patch_indices])
     return slices
+
+
+def add_non_spatial_dims(spatial_data_size_a, data_size_b, spatial_size, spatial_first):
+    non_spatial_dims = len(data_size_b) - len(spatial_size)
+    if non_spatial_dims > 0 and spatial_first:
+        non_spatial_dims = data_size_b[len(spatial_size):]
+        data_size_a = (*spatial_data_size_a, *non_spatial_dims)
+        return data_size_a
+    elif non_spatial_dims > 0:
+        non_spatial_dims = data_size_b[:-len(spatial_size)]
+        data_size_a = (*non_spatial_dims, *spatial_data_size_a)
+        return data_size_a
+    else:
+        return spatial_data_size_a
+    
+
+def broadcast_to(data, target_shape, spatial_first):
+    if spatial_first:
+        non_spatial_dims = len(target_shape) - len(data.shape)
+        data_reshaped = data
+        for _ in range(non_spatial_dims):
+            data_reshaped = data_reshaped[..., np.newaxis]
+        data_reshaped = np.broadcast_to(data_reshaped, target_shape)
+    else:
+        data_reshaped = np.broadcast_to(data, target_shape)
+    data_reshaped = np.copy(data_reshaped)
+    return data_reshaped
